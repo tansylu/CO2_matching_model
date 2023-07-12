8ae2196dafeda2ece9a6b1a6480de6bd0626734e
Danilo <danilo.hervasproano@gmail.com>
2023-07-12 14:37:18 +0300

    Danilo

--- start.py
+++ start.py
@@ -32,3 +32,21 @@ for i in range(len(sentences1)):
     for j in range(len(sentences2)):
         if(cosine_scores[i][j] >0.7):
             print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[j], cosine_scores[i][j]))
+
+
+
+# Single list of sentences - Possible tens of thousands of sentences
+sentences = ['The cat sits outside',
+             'A man is playing guitar',
+             'I love pasta',
+             'The new movie is awesome',
+             'The cat plays in the garden',
+             'A woman watches TV',
+             'The new movie is so great',
+             'Do you like pizza?']
+
+paraphrases = util.paraphrase_mining(model, sentences, corpus_chunk_size=len(sentences), top_k=1)
+
+for paraphrase in paraphrases[0:10]:
+    score, i, j = paraphrase
+    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], score))
