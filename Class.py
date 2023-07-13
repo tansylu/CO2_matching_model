a73cfece6517dc914ac9fffc5888ce2e4a618440
DaniDanilo <139151569+DaniDanilo@users.noreply.github.com>
2023-07-13 20:00:34 +0300

    Add files via upload
    Testing approach with OOP, managing to get a proper result with one of the models for paraphrasing

---
+++ Class.py
@@ -0,0 +1,40 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+import pickle
+
+class string_embedder:
+    def __init__(self, MODEL):
+        """
+
+        :rtype: object
+        """
+        # MODEL = 'all-MiniLM-L6-v2' a different model to test out
+        # initialize the embedder transformer class and use a single variable
+        self.model = SentenceTransformer(MODEL)
+
+    def embed_list_of_strings(self, list_of_strings: [str]):
+        #### embedding of a list of words - non topic specific
+        #### to be used with the function get_closest_from_list_of_strings to get the closest term
+        if not type(list_of_strings) == 'list':
+            list_of_strings = list(list_of_strings)
+            
+        self.list_of_strings= list_of_strings
+        self.embeddings = self.model.encode(self.list_of_strings)
+
+
+    def get_closest_from_list_of_strings(self, item: str):
+        #### returns the closest term to the string item from the list that was sent to 'embed_list_of_strings'
+        val = util.cos_sim(self.embeddings, self.model.encode(item))
+        amax = np.argmax(val)
+        return self.list_of_strings[amax]
+    
+
+
+#load data set of matched products
+data_categories=pickle.load(open("product_EI_to_EX.pickle", "rb"))
+
+model = string_embedder('paraphrase-MiniLM-L6-v2')
+
+model.embed_list_of_strings(data_categories)
+match=model.get_closest_from_list_of_strings('beef')
+print(match)
