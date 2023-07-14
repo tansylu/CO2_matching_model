179f0d36eb687eedbf9e21d1731211de2b91f85d
tansylu <tansiluakh@gmail.com>
2023-07-14 14:29:02 +0200

    incorporate codex

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
 from model_class import string_embedder
-from pickle_processor import ex_to_ei_flat, ei_to_ex_flat
+import pandas as pd
+from pickle_processor import init_processing
+
+##run this once to obtain the pickle-excel convesion, otherwise skip
+init_processing()
+
+# Read the codex Excel file
+df1 = pd.read_excel('codex.xlsx')
+
+# Access the columns in the first dataframe
+code_column = df1['Code']
+indented_tree_column = df1['Product']
+
+# Read the second Excel file
+df2 = pd.read_excel('output_ex_to_ei.xlsx')
+
+# Access the columns in the second dataframe
+subcategory_column = df2['Subcategory']
+parent_category_column = df2['Parent Category']
+
+# Initialize the string_embedder model
 model = string_embedder('paraphrase-MiniLM-L6-v2')
 
-model.embed_list_of_strings(ex_to_ei_flat)
-match = model.get_closest_from_list_of_strings('newspaper')
-print(match)
+# Create a new dataframe to store the results
+result_df = pd.DataFrame(columns=['Code', 'Product', 'Matched Subcategory', 'Parent Category'])
+
+print("Running main.py...")
+
+# Loop through each item in the "Product" column
+for product in indented_tree_column:
+    # Embed the product string and get the closest match
+    model.embed_list_of_strings(subcategory_column.tolist())
+    match = model.get_closest_from_list_of_strings(product)
+    
+    # Find the index of the matched subcategory
+    match_index = subcategory_column[subcategory_column == match].index[0]
+    
+    # Get the parent category based on the match index
+    parent_category = parent_category_column[match_index]
+    
+    # Append the result to the result dataframe
+    result_df = pd.concat([result_df, pd.DataFrame({
+        'Code': [code_column[match_index]],
+        'Product': [product],
+        'Matched Subcategory': [match],
+        'Parent Category': [parent_category]
+    })], ignore_index=True)
+
+print("Prediction complete.")
+print(result_df)
+
+# Save the result dataframe to an Excel file
+result_df.to_excel('result.xlsx', index=False)
--- pickle_processor.py
+++ pickle_processor.py
@@ -37,19 +37,19 @@ def output_to_excel(flattened_list, inverse_matching, name):
     # Save the workbook
     workbook.save('output_{n}.xlsx'.format(n=name))
 
+def init_processing():
+    # Load the matched product data from pickle files
+    ex_to_ei = pickle.load(open("product_EX_to_EI.pickle", "rb"))
+    ei_to_ex = pickle.load(open("product_EI_to_EX.pickle", "rb"))
 
-# Load the matched product data from pickle files
-ex_to_ei = pickle.load(open("product_EX_to_EI.pickle", "rb"))
-ei_to_ex = pickle.load(open("product_EI_to_EX.pickle", "rb"))
+    # Create inverse matching dictionaries to determine parent category by looking at subcategory
+    inverse_matching_ex_to_ei = create_inverse_matching(ex_to_ei)
+    inverse_matching_ei_to_ex = create_inverse_matching(ei_to_ex)
 
-# Create inverse matching dictionaries to determine parent category by looking at subcategory
-inverse_matching_ex_to_ei = create_inverse_matching(ex_to_ei)
-inverse_matching_ei_to_ex = create_inverse_matching(ei_to_ex)
+    # Obtain flattened value lists for both dictionaries
+    ex_to_ei_flat = flatten_values_from_pickle(ex_to_ei)
+    ei_to_ex_flat = flatten_values_from_pickle(ei_to_ex)
 
-# Obtain flattened value lists for both dictionaries
-ex_to_ei_flat = flatten_values_from_pickle(ex_to_ei)
-ei_to_ex_flat = flatten_values_from_pickle(ei_to_ex)
-
-# Output the flattened lists to Excel with parent categories
-output_to_excel(ex_to_ei_flat, inverse_matching_ex_to_ei, "ex_to_ei")
-output_to_excel(ei_to_ex_flat, inverse_matching_ei_to_ex, "ei_to_ex")
+    # Output the flattened lists to Excel with parent categories
+    output_to_excel(ex_to_ei_flat, inverse_matching_ex_to_ei, "ex_to_ei")
+    output_to_excel(ei_to_ex_flat, inverse_matching_ei_to_ex, "ei_to_ex")
