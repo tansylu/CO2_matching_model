00ef1a85f6f70fc44b00838f8ab988a31999e72b
Danilo <danilo.hervasproano@gmail.com>
2023-07-19 13:45:55 +0300

    Removed data differnce getting script

--- main.py
+++ main.py
@@ -67,15 +67,3 @@ print(result_df)
 result_df.to_excel('result_modelAll.xlsx', index=False)
 
 
-# Load the Excel files into pandas dataframes
-model1 = pd.read_excel('result_modelPara.xlsx')
-model2 = pd.read_excel('result_modelAll.xlsx')
-
-# Compare the dataframes and identify the rows that do not match
-differences = model1.compare(model2)
-
-# Filter out the rows that match and keep only the differing rows
-differing_rows = differences[differences['self'].notna()]
-
-# Save the differing rows to a new Excel file
-differences.to_excel('differences.xlsx', index=True)
