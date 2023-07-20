a6f7ad210ca60c8fbed8412d1459a4a6c7e68470
Danilo <danilo.hervasproano@gmail.com>
2023-07-20 14:14:05 +0300

    More understandable excel file output

--- difference_getter.py
+++ difference_getter.py
@@ -5,11 +5,8 @@ model1 = pd.read_excel('result_modelPara.xlsx')
 model2 = pd.read_excel('result_modelAll.xlsx')
 
 
-# Compare the dataframes and identify the rows that do not match
-differences = model1.compare(model2)
-
-# Filter out the rows that match and keep only the differing rows
-#differing_rows = differences[differences['self'].notna()] this gives error, need to check
+merged = model1.merge(model2, indicator=True, how='outer')
+differing_rows = merged[merged['_merge'] != 'both'].drop(columns=['_merge'])
 
 # Save the differing rows to a new Excel file
-differences.to_excel('differences.xlsx', index=True)
+differing_rows.to_excel('differences_new.xlsx', index=False) #Need to filter in alphabetical order to be able to see the 2 models' output together
